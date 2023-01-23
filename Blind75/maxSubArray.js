09d535f0367043bf2bcb6e5debf3bb8d1299a4f7
tfmcadam <emergin25@gmail.com>
2023-01-23 08:43:16 -0800

    changed for loop

--- Blind75/maxSubArray.js
+++ Blind75/maxSubArray.js
@@ -16,15 +16,20 @@ var maxSubArray = function (nums){
 }
 console.log(maxSubArray(nums))
 
+
+nums2 = [2, -2, 3, 5]
 var subArray = function (nums){
     let currentSum = nums[0]
     let maxSum = currentSum
     const n = nums.length
-    for ( let i = 0; i < n; i++){
+    for ( let i = 1; i < n; i++){
         currentSum = Math.max(nums[i], currentSum + nums[i]);
+        console.log(currentSum)
         maxSum = Math.max(maxSum, currentSum);
+        console.log(maxSum)
     }
     return maxSum;
 }
 
 console.log(subArray(nums))
+console.log(subArray(nums2))
