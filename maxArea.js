45728de683ca82aaaeaaf6028c746decc1ce5121
tfmcadam <emergin25@gmail.com>
2023-01-31 13:00:22 -0800

    added maxArea

---
+++ maxArea.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+
+Input: height2 = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+// Output: 49
+
+Input: height3 = [1, 1]
+// Output: 1
+
+
+var maxArea = function(height){
+    let i = 0
+    let j = height.length - 1
+    let max = 1
+    let area
+
+    while (i < j){
+        area = (j-1) * Math.min(height[i], height[j])
+        max = Math.max(area, max)
+        height[i] < height[j] ? i++ : j--;
+    }
+    return max
+};
+
+console.log(height2)
+console.log(maxArea(height3))
+console.log(maxArea(height2))
+
