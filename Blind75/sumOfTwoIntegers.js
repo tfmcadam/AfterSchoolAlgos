7b67be22102851ef0b77e2aa7ffff52d1a1d984c
tfmcadam <emergin25@gmail.com>
2023-02-02 14:09:47 -0800

    minor update to code

--- Blind75/sumOfTwoIntegers.js
+++ Blind75/sumOfTwoIntegers.js
@@ -9,11 +9,17 @@ Input: a = 1, b = 2
 Inpu1: a = 2, b = 3
 // Output: 5
 
+// this function breaks the numbers into binary addition operation 
+
 var getSum = function (a, b) {
     let carry;
+    // repeat the addition opperation until "a" becomes 0
     while (a !== 0){
+        // calculate the carry using the bitwise AND
         carry = a & b;
+        // calculate the sum  of "a" and "b" using bitwise XOR
         b = a ^ b;
+        // shift the carry left one bit
         a = carry << 1;
     }
     return b
